15ff248ae3c33b90618e8e082af9d5fd23aa82e0
Arin <arinarjani@gmail.com>
2017-01-25 11:13:41 -0800

    added functions

--- pa08.cpp
+++ pa08.cpp
@@ -16,16 +16,6 @@
 // not need for std::cin for instance
 using namespace std;
 
-// prototypes
-// ISSUE: HOW DO I PASS IN THE SIZES OF THE ARRAY WITHOUT KNOWING
-//        THE SIZE FROM THE START?  I HAVE TO GET IT FROM THE FILE.
-//        THIS MEANS I CANNOT USE A CONSTANT OR EVEN DECLARE PROTOTYPES RIGHT?
-// void rowSwap( int inputArr[][], int size);
-// void colSwap();
-// void incrementByOne();
-// void decrementByOne();
-// void transpose();
-
 // *************************************************
 //                 TODO
 // *************************************************
@@ -34,6 +24,16 @@ using namespace std;
 // 3) right now your program can only run 1 test case, after that it scans
 //    for another test case number and repreats.  it cannot do multiple
 //    operations as well; only one operation really well
+
+const int SIZE_OF_ROW = 10;
+const int SIZE_OF_COL = 10;
+
+// PROTOTYPES
+void rowSwap( int inputArr[][SIZE_OF_COL], int sizeOfRow, int opp1, int opp2);
+void colSwap( int inputArr[][SIZE_OF_COL], int sizeOfRow, int opp1, int opp2);
+void incrementByOne( int inputArr[][SIZE_OF_COL], int sizeOfRow );
+void decrementByOne( int inputArr[][SIZE_OF_COL], int sizeOfRow );
+void transpose( int inputArr[][SIZE_OF_COL], int holder[][SIZE_OF_COL], int sizeOfRow );
 
 int main() {
     int testCases;
@@ -45,6 +45,8 @@ int main() {
     char currentValOfArr;
     int startOfCharNumbers = 48;
     int timesAround;
+    int mainArray[SIZE_OF_ROW][SIZE_OF_COL];
+    int arrHolder[SIZE_OF_ROW][SIZE_OF_COL];
 
     cin >> testCases >> dimensionOfMatrix;
 
@@ -62,17 +64,6 @@ int main() {
 
         cout << "testCases: " << testCases << endl;
         cout << "dimesionsOfMatix: " << dimensionOfMatrix<< endl;
-
-        // declare array with size given
-        int mainArray[dimensionOfMatrix][dimensionOfMatrix];
-
-        // mainArray holders for each row/col
-        int firstRowColSwitch[dimensionOfMatrix];
-        int secondRowColSwitch[dimensionOfMatrix];
-
-        // array for when I transpose
-        int arrHolder[dimensionOfMatrix][dimensionOfMatrix];
-
 
         // fill mainArray with values given
         for ( int i = 0; i < dimensionOfMatrix; i++ ) {
@@ -104,10 +95,11 @@ int main() {
         }
 
         // for testing purposes
-         cout << "rowColOppOne: " << rowColOppOne << endl;
-         cout << "rowColOppTwo: " << rowColOppTwo << endl;
+        cout << "rowColOppOne: " << rowColOppOne << endl;
+        cout << "rowColOppTwo: " << rowColOppTwo << endl;
 
         if ( operation == "row" ) {
+            // **
             // {
             //   {1, 2, 3, 4}
             //   {5, 6, 7, 8}
@@ -115,26 +107,7 @@ int main() {
             //   {4, 5, 6, 7}
             // }
             // want to switch row 2 with row 4
-
-            // populate the array holder firstRowSwitch
-            for ( int k = 0; k < dimensionOfMatrix; k++ ) {
-                firstRowColSwitch[k] = mainArray[rowColOppOne - 1][k];
-            }
-
-            // populate the array holder secondRowSwitch
-            for ( int q = 0; q < dimensionOfMatrix; q++ ) {
-                secondRowColSwitch[q] = mainArray[rowColOppTwo - 1][q];
-            }
-
-            // make the switch on first row
-            for (int a = 0; a < dimensionOfMatrix; a++) {
-                mainArray[rowColOppOne - 1][a] = secondRowColSwitch[a];
-            }
-
-            // make the switch o second row
-            for (int b = 0; b < dimensionOfMatrix; b++) {
-                mainArray[rowColOppTwo - 1][b] = firstRowColSwitch[b];
-            }
+            rowSwap( mainArray, dimensionOfMatrix, rowColOppOne, rowColOppTwo );
         }
 
         if ( operation == "col" ) {
@@ -145,95 +118,45 @@ int main() {
           //   {4, 5, 6, 7}
           // }
           // want to switch col 2 with row 4
-          // populate the array holder firstRowColSwitch
-          for ( int k = 0; k < dimensionOfMatrix; k++ ) {
-              firstRowColSwitch[k] = mainArray[k][rowColOppOne - 1];
-          }
-
-          // populate the array holder secondRowColSwitch
-          for ( int q = 0; q < dimensionOfMatrix; q++ ) {
-              secondRowColSwitch[q] = mainArray[q][rowColOppTwo - 1];
-          }
-          // make the switch on first col
-          for (int a = 0; a < dimensionOfMatrix; a++) {
-              mainArray[a][rowColOppOne - 1] = secondRowColSwitch[a];
-          }
-
-          // make the switch o second col
-          for (int b = 0; b < dimensionOfMatrix; b++) {
-              mainArray[b][rowColOppTwo - 1] = firstRowColSwitch[b];
-          }
+            colSwap( mainArray, dimensionOfMatrix, rowColOppOne, rowColOppTwo );
         }
 
         if ( operation == "inc" ) {
-          // {
-          //   {1, 2, 3, 4}
-          //   {5, 6, 7, 8}
-          //   {9, 1, 2, 3}
-          //   {4, 5, 6, 7}
-          // }
-          // want to incremtent everything by 1 to get:
-          // {
-          //   {2, 3, 4, 5}
-          //   {6, 7, 8, 9}
-          //   {1, 2, 3, 4}
-          //   {5, 6, 7, 8}
-          // }
-
-          for ( int i = 0; i < dimensionOfMatrix; i++ ) {
-              int numReset = 0;
-              for ( int j = 0; j < dimensionOfMatrix; j++ ) {
-                  int currentVal = mainArray[i][j];
-                  cout << "currentVal: " << currentVal << endl;
-                  if ( ( currentVal + 1 ) % 10 == 0 ) {
-                      mainArray[i][j] = numReset;
-                  } else {
-                      currentVal++;
-                      cout << "currentVal++: " << currentVal << endl;
-                      mainArray[i][j] = currentVal;
-                  }
-              }
-          }
-
-
+            // {
+            //   {1, 2, 3, 4}
+            //   {5, 6, 7, 8}
+            //   {9, 1, 2, 3}
+            //   {4, 5, 6, 7}
+            // }
+            // want to incremtent everything by 1 to get:
+            // {
+            //   {2, 3, 4, 5}
+            //   {6, 7, 8, 9}
+            //   {1, 2, 3, 4}
+            //   {5, 6, 7, 8}
+            // }
+            incrementByOne( mainArray, dimensionOfMatrix );
         }
 
         if ( operation == "dec" ) {
-          for ( int i = 0; i < dimensionOfMatrix; i++ ) {
-              int numReset = 0;
-              for ( int j = 0; j < dimensionOfMatrix; j++ ) {
-                  int currentVal = mainArray[i][j];
-                  cout << "currentVal: " << currentVal << endl;
-                  if ( ( currentVal - 1 ) % 10 == -1 ) {
-                      mainArray[i][j] = numReset;
-                  } else {
-                      currentVal--;
-                      cout << "currentVal--: " << currentVal << endl;
-                      mainArray[i][j] = currentVal;
-                  }
-              }
-          }
+            decrementByOne( mainArray, dimensionOfMatrix );
         }
 
         if ( operation == "transpose" ) {
-          // {
-          //   {1, 2, 3, 4}
-          //   {5, 6, 7, 8}
-          //   {9, 1, 2, 3}
-          //   {4, 5, 6, 7}
-          // }
-          // after transposing:
-          // {
-          //   {1, 5, 9, 4}
-          //   {2, 6, 1, 5}
-          //   {3, 7, 2, 6}
-          //   {4, 8, 3, 7}
-          // }
-            for ( int i = 0; i < dimensionOfMatrix; i++ ) {
-                for ( int j = 0; j < dimensionOfMatrix; j++ ) {
-                    arrHolder[j][i] = mainArray[i][j];
-                }
-            }
+            // {
+            //   {1, 2, 3, 4}
+            //   {5, 6, 7, 8}
+            //   {9, 1, 2, 3}
+            //   {4, 5, 6, 7}
+            // }
+            // after transposing:
+            // {
+            //   {1, 5, 9, 4}
+            //   {2, 6, 1, 5}
+            //   {3, 7, 2, 6}
+            //   {4, 8, 3, 7}
+            // }
+            transpose( mainArray, arrHolder, dimensionOfMatrix );
         }
 
          // for testing purposes
@@ -259,4 +182,83 @@ int main() {
  }
 
   return 0;
+}
+
+// FUNCTIONS
+void rowSwap( int inputArr[][SIZE_OF_COL], int sizeOfRow, int opp1, int opp2) {
+    // array holders for each row/col
+    int firstRowColSwitch[SIZE_OF_ROW];
+    int secondRowColSwitch[SIZE_OF_ROW];
+
+    // populate the array holders
+    for ( int k = 0; k < sizeOfRow; k++ ) {
+        firstRowColSwitch[k] = inputArr[opp1 - 1][k];
+        secondRowColSwitch[k] = inputArr[opp2 - 1][k];
+    }
+
+    // make the row switches on the inputArr
+    for (int a = 0; a < sizeOfRow; a++) {
+        inputArr[opp1 - 1][a] = secondRowColSwitch[a];
+        inputArr[opp2 - 1][a] = firstRowColSwitch[a];
+    }
+}
+
+void colSwap( int inputArr[][SIZE_OF_COL], int sizeOfRow, int opp1, int opp2) {
+    // array holders for each row/col
+    int firstRowColSwitch[SIZE_OF_ROW];
+    int secondRowColSwitch[SIZE_OF_ROW];
+
+    // populate the array holders
+    for ( int k = 0; k < sizeOfRow; k++ ) {
+        firstRowColSwitch[k] = inputArr[k][opp1 - 1];
+        secondRowColSwitch[k] = inputArr[k][opp2 - 1];
+    }
+
+    // make the row switches on the inputArr
+    for (int a = 0; a < sizeOfRow; a++) {
+        inputArr[a][opp1 - 1] = secondRowColSwitch[a];
+        inputArr[a][opp2 - 1] = firstRowColSwitch[a];
+    }
+}
+
+void incrementByOne( int inputArr[][SIZE_OF_COL], int sizeOfRow ) {
+    for ( int i = 0; i < sizeOfRow; i++ ) {
+        int numReset = 0;
+        for ( int j = 0; j < sizeOfRow; j++ ) {
+            int currentVal = inputArr[i][j];
+            cout << "currentVal: " << currentVal << endl;
+            if ( ( currentVal + 1 ) % 10 == 0 ) {
+                inputArr[i][j] = numReset;
+            } else {
+                currentVal++;
+                cout << "currentVal++: " << currentVal << endl;
+                inputArr[i][j] = currentVal;
+            }
+        }
+    }
+}
+
+void decrementByOne( int inputArr[][SIZE_OF_COL], int sizeOfRow ) {
+    for ( int i = 0; i < sizeOfRow; i++ ) {
+        int numReset = 0;
+        for ( int j = 0; j < sizeOfRow; j++ ) {
+            int currentVal = inputArr[i][j];
+            cout << "currentVal: " << currentVal << endl;
+            if ( ( currentVal - 1 ) % 10 == -1 ) {
+                inputArr[i][j] = numReset;
+            } else {
+                currentVal--;
+                cout << "currentVal--: " << currentVal << endl;
+                inputArr[i][j] = currentVal;
+            }
+        }
+    }
+}
+
+void transpose( int inputArr[][SIZE_OF_COL], int holder[][SIZE_OF_COL], int sizeOfRow ) {
+  for ( int i = 0; i < sizeOfRow; i++ ) {
+      for ( int j = 0; j < sizeOfRow; j++ ) {
+          holder[j][i] = inputArr[i][j];
+      }
+  }
 }
